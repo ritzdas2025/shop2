1e42f8a059d288eb597e5fafdb220f10883fc095
ritzdas2025 <ritzdas2025@zohomail.in>
2025-12-15 11:46:03 +0000

    chore: save full website to main

---
+++ next.config.ts
@@ -0,0 +1,33 @@
+import type {NextConfig} from 'next';
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default withBundleAnalyzer(nextConfig);
